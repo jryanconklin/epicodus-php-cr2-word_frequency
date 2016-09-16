b938df5cd1c7802d2626e0174c4f1c10f7f849fa
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-16 08:50:46 -0700

    Write countPhrase Function to Pass Specification 1.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -10,6 +10,17 @@
     // General Methods
         function countRepeats($user_word, $user_phrase)
         {
+            $filter_word = $user_word;
+            $filter_phrase = $user_phrase;
+            $filter_phrase_array = explode(' ', $filter_phrase);
+            $count = 0;
+
+            foreach($filter_phrase_array as $word) {
+                if ($word == $filter_word) {
+                    $count++;
+                }
+                return $count;
+            }
 
 
 
