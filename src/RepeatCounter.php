b7705aecb7cd077ac57513eb1c4453207d7f28e9
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-16 10:06:00 -0700

    Add filter_var to User Inputs to Sanitize Strings.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -11,8 +11,10 @@
         function countRepeats($user_word, $user_phrase)
         {
             $strip_punctuation = array(".", "!", "?", ",", ":", ";");
-            $filter_word = strtolower($user_word);
-            $filter_phrase = strtolower($user_phrase);
+            $filter_word = filter_var($user_word, FILTER_SANITIZE_STRING);
+            $filter_word = strtolower($filter_word);
+            $filter_phrase = filter_var($user_phrase, FILTER_SANITIZE_STRING);
+            $filter_phrase = strtolower($filter_phrase);
             $filter_phrase = str_replace($strip_punctuation, "", $filter_phrase);
             $filter_phrase_array = explode(' ', $filter_phrase);
             $count = 0;
