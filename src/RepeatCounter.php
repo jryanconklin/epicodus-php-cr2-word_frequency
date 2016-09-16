af60c57dd66ef912718cad6cd8512233365f6f5d
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-16 10:00:17 -0700

    Update countRepeats to solve for Spec 8 - Punctuation is Removed Before Comparing.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -10,8 +10,10 @@
     // General Methods
         function countRepeats($user_word, $user_phrase)
         {
+            $strip_punctuation = array(".", "!", "?", ",", ":", ";");
             $filter_word = strtolower($user_word);
             $filter_phrase = strtolower($user_phrase);
+            $filter_phrase = str_replace($strip_punctuation, "", $filter_phrase);
             $filter_phrase_array = explode(' ', $filter_phrase);
             $count = 0;
 
